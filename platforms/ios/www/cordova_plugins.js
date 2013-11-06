d5ae6b9e90b9ff359d6a511bbb7d8ba2ba12a5a3
Nicolas Leclerc <nl@spirotron.fr>
2013-11-06 12:50:58 +0100

    Replaced plugin code with customized one.

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -15,33 +15,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.chromium.common/events.js",
-        "id": "org.chromium.common.events",
-        "clobbers": [
-            "chrome.Event"
-        ]
-    },
-    {
-        "file": "plugins/org.chromium.common/errors.js",
-        "id": "org.chromium.common.errors"
-    },
-    {
-        "file": "plugins/org.chromium.common/stubs.js",
-        "id": "org.chromium.common.stubs"
-    },
-    {
-        "file": "plugins/org.chromium.common/helpers.js",
-        "id": "org.chromium.common.helpers"
-    },
-    {
-        "file": "plugins/org.chromium.common/lib/CryptoJS/sha256.js",
-        "id": "org.chromium.common.CryptoJS-sha256"
-    },
-    {
-        "file": "plugins/org.chromium.common/lib/CryptoJS/enc-base64-min.js",
-        "id": "org.chromium.common.CryptoJS-enc-base64-min"
-    },
-    {
         "file": "plugins/org.chromium.socket/socket.js",
         "id": "org.chromium.socket.Socket",
         "clobbers": [
